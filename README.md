191a1cae38f956b22f5333af799a22bef1f19251
Oky Mikhael <okymikhael@gmail.com>
2022-03-14 21:32:30 +0700

    New update assets and structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Build with Tailwind
+
+Missing :
+- "Choose your best wedding place!" text [page home]
+- Catalog button image [page detail]
+- Catalog button link target [page detail]
+
+Best result
+- Samsung S8+
+- Iphone SE
+- Microsoft Lumia 950
---
+++ assets/js/paging.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.page-detail').css({'display':'none'});
+    
+    $('#jkt').click(function(){
+        $('.page-home').fadeOut('fast');
+        $('.page-detail').delay(400).fadeIn();
+
+        $('#link-catalog').attr("href", "/");
+        $('#link-instagram').attr("href", "https://www.instagram.com/trinity_artwork");
+        $('#link-whatsapp').attr("href", "https://wa.me/+6281564601750");
+        $('#link-whatsapp img').attr("src", "/assets/image/button contact Jakarta.png");
+    })
+
+    $('#bdg').click(function(){
+        $('.page-home').fadeOut('fast');
+        $('.page-detail').delay(400).fadeIn();
+
+        $('#link-catalog').attr("href", "/");
+        $('#link-instagram').attr("href", "https://www.instagram.com/trinity_artwork");
+        $('#link-whatsapp').attr("href", "https://wa.me/+6281564601750");
+        $('#link-whatsapp img').attr("src", "/assets/image/button contact Bandung.png");
+    })
+
+    $('#dps').click(function(){
+        $('.page-home').fadeOut('fast');
+        $('.page-detail').delay(400).fadeIn();
+
+        $('#link-catalog').attr("href", "/");
+        $('#link-instagram').attr("href", "https://www.instagram.com/trinity_artwork");
+        $('#link-whatsapp').attr("href", "https://wa.me/+6281564601750");
+        $('#link-whatsapp img').attr("src", "/assets/image/button contact Bali.png");
+    })
+})
